0d28be8a135d7fee99601e6025fc0aa738cfc85f
Esdras Ribeiro <esdrasribeiro45@gmail.com>
2024-09-25 21:37:46 -0300

    first commit

---
+++ src/somaNumeros.js
@@ -0,0 +1,37 @@
+const num1 = 5
+const num2 = 10
+const soma = num1 + num2
+
+console.log(`A soma é ${soma}`)
+
+// Métodos HTTP
+
+// GET - Pegar informações da API
+
+// POST - Adicionar informações na API
+
+// PUT - Atualizar informações na API
+
+// DELETE - Remover informações na API
+
+// C - POST 
+// R - GET 
+// U - PUT 
+// D - DELETE
+
+
+// JSON
+// {
+//     "tarefas": [
+//         {
+//             "id": 1,
+//             "descicao": 'Ir ao mercado.',
+//             "concluida": false
+//         }
+//         {
+//             "id": 2,
+//             "descicao": 'Estudar Node',
+//             "concluida": true
+//         }
+//     ]
+// }
